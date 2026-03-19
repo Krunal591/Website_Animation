69b28ced5e710b72f15c662820b8260e08202fb2
Krunal591 <krunalprajapati50974@gmail.com>
2026-03-19 20:31:10 +0530

    Projects add card

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ProjectCard = () => {
+    return (
+        <>
+            <div className='w-full h-\[700px\]  mb-4 flex gap-4'>
+                <div className='w-1/2 transition relative rounded-none overflow-hidden hover:rounded-[50px] h-full group'>
+                    <img className='h-full w-full object-cover' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt='' />
+                    <div className='absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10 opacity-0 group-hover:opacity-100 transition'>
+                        <h2 className='uppercase text-6xl font-[font1] border-4 pt-4 px-8 text-white border-white rounded-full'>View Project</h2>
+                    </div>
+                </div>
+                <div className='w-1/2 transition relative rounded-none overflow-hidden hover:rounded-[50px] h-full group'>
+                    <img className='h-full w-full object-cover' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt='' />
+                    <div className='absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10 opacity-0 group-hover:opacity-100 transition'>
+                        <h2 className='uppercase text-6xl font-[font1] border-4 pt-4 px-8 text-white border-white rounded-full'>View Project</h2>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectCard
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import ProjectCard from '../components/Projects/ProjectCard'
 
 const Projects = () => {
+  const project = ['https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b','https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e','https://k72.ca/images/caseStudies/OKA/OKA_thumbnail.jpg?w=1280&h=960&s=c12c27c9db3c521e4c82a246a8d5c022','https://k72.ca/images/caseStudies/Opto_Reseau_Brand/opto_thumbnail2.jpg?w=1280&h=960&s=7f23d7d824eb04c784dcf51380fe3996','https://k72.ca/images/caseStudies/COUP_FUMANT/CF_thumbnail.jpg?w=1280&h=960&s=c119303a20520c4188aa3f592038fd4c','https://k72.ca/images/caseStudies/SHELTON/thumbnailimage_shelton.jpg?w=1280&h=960&s=63d0eaa180cbc02d3ada285ad9ef1479','https://k72.ca/images/caseStudies/BEST/BEST_site_Thumbnail.jpg?w=1280&h=960&s=2b73eecfda8d95a72efa768383b50860']
   return (
     <div className='p-4'>
       <div className='pt-[45vh]'>
         <h2 className='font-[font2] text-[9vw] uppercase'>Projects</h2>
       </div>
       <div className='-mt-13'>
-        <div className='w-full h-\[700px\]  mb-4 flex gap-4'>
-          <div className='w-1/2 transition relative rounded-none overflow-hidden hover:rounded-[50px] h-full '>
-            <img className='h-full w-full object-cover' src='https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290.jpg?w=640&h=290&s=ac50a70feaaa2601b3aacad544c6045b' alt='' />
-            <div className='absolute top-0 flex items-center justify-center left-0 h-full w-full bg-black/10'>
-              <h2 className='uppercase text-6xl font-[font1] border-4 pt-4 px-8 text-white border-white rounded-full'>View Project</h2>
-            </div>
-          </div>
-        </div>
+        <ProjectCard/>
+        <ProjectCard/>
       </div>
     </div>
   )
