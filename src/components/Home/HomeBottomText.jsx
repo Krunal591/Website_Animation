31ebc62912a6421010634aecec909c35c01932bb
Krunal591 <krunalprajapati50974@gmail.com>
2026-03-23 21:07:16 +0530

    responsive navbar and home page

--- src/components/Home/HomeBottomText.jsx
+++ src/components/Home/HomeBottomText.jsx
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom'
 const HomeBottomText = () => {
   return (
     <div className='font-[font2] flex items-center justify-center gap-2 text-white'>
-      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-40 flex items-center px-14 border-white rounded-full uppercase'>
-      <Link className='text-[6vw] mt-6' to='/projects'>Projects</Link>
+      <p className='absolute lg:w-[25vw] w-80 right-0 bottom-50 font-[font-1] lg:text-md text-base lg:leading-relaxed leading-tight'>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.</p>
+      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-40 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
+      <Link className='text-[6vw] lg:mt-6' to='/projects'>Projects</Link>
       </div>
-      <div className='border-3 hover:border-[#D3FD50] hover:text-[#D3FD50] h-40 flex items-center px-14 border-white rounded-full uppercase'>
-      <Link className='text-[6vw] mt-6' to='/agence'>Agence</Link>
+      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] lg:h-40 flex items-center px-3 pt-1 lg:px-14 border-white rounded-full uppercase'>
+      <Link className='text-[6vw] lg:mt-6' to='/agence'>Agence</Link>
       </div>
     </div>
   )
--- src/components/Home/HomeHerotext.jsx
+++ src/components/Home/HomeHerotext.jsx
@@ -3,16 +3,16 @@ import Video from './Video'
 
 const HomeHerotext = () => {
   return (
-    <div className='font-[font-2] pt-5 text-center text-white'>
-      <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[8vw]'>
+    <div className='font-[font-2] mt-72 lg:mt-0 pt-5 text-center text-white'>
+      <div className='lg:text-[9.5vw] text-[12vw] uppercase flex items-center justify-center lg:leading-[8vw] leading-[10vw]'>
         The spark for
       </div>
-      <div className='text-[9.5vw] flex items-center justify-center uppercase leading-[8vw]'>
+      <div className='lg:text-[9.5vw] text-[12vw] flex items-center justify-center uppercase lg:leading-[8vw] leading-[10vw]'>
         all
         <div className='h-[8vw] w-[16vw] rounded-full -mt-3 overflow-hidden'><Video/></div>
         things
       </div>
-      <div className='text-[9.5vw] uppercase flex items-center justify-center leading-[8vw]'>
+      <div className='lg:text-[9.5vw] text-[12vw] uppercase flex items-center justify-center lg:leading-[8vw] leading-[10vw]'>
         creative
       </div>
     </div>
